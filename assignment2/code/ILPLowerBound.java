6d01825ad950f20944aed8002d2a22be6db9f8f5
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2013-05-16 19:15:39 +0200

    stuff for ILP

--- assignment2/code/ILPLowerBound.java
+++ assignment2/code/ILPLowerBound.java
@@ -1,4 +1,5 @@
 import lpsolve.*;
+import java.io.*;
 
 class ILPLowerBound {
     public static double lowerbound(TCPProblem p, BnBNode n) {
@@ -7,14 +8,56 @@ class ILPLowerBound {
             int numEdges = numVertices * (numVertices-1);
             int numVars = numVertices + numEdges;
 
-            LpSolve solver = LpSolve.makeLp(0, numVars);
+            File program = File.createTempFile("ilp", ".lp");
+            BufferedWriter pw = new BufferedWriter( new FileWriter( program ) );
 
+            // File format: http://lpsolve.sourceforge.net/5.5/lp-format.htm
 
+            pw.write("min: " + objective(p) + ";\n");
+            pw.write("int " + intvars(p) + ";\n");
+
+            pw.flush();
+            LpSolve solver = LpSolve.readLp(program.getPath(), LpSolve.NORMAL, null);
+            // Do stuff with linear program.
             solver.deleteLp();
+            program.delete();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return 0;
+    }
+
+    private static String objective(TCPProblem p) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < p.n; i++) {
+            for (int j = 0; j < p.n; j++) {
+                if (i==j) continue;
+
+                sb.append("+ ").append(p.vertices[i].distance(p.vertices[j]))
+                  .append(" e_").append(i).append("_").append(j).append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static String intvars(TCPProblem p) {
+        boolean begin = true;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < p.n; i++) {
+            for (int j = 0; j < p.n; j++) {
+                if (i==j) continue;
+                if (begin) {
+                    begin = false;
+                } else {
+                    sb.append(", ");
+                }
+
+                sb.append(" e_").append(i).append("_").append(j).append(" ");
+            }
+        }
+
+        return sb.toString();
     }
 }
--- assignment2/code/run.sh
+++ assignment2/code/run.sh
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH=/usr/lib/lp_solve
 
-javac -cp ".:lib/lpsolve55j.jar" *.java
+javac -cp ".:lib/lpsolve55j.jar" *.java && \
 java -cp ".:lib/lpsolve55j.jar" BnB
